394bb48a358f1a9bf87734ccebb1c84c2d58a3b8
aoa01691 <abdulabubakar@my.unt.edu>
2026-03-06 17:23:33 -0600

    Created main.cpp and set Simple key-value pair struct

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+// Simple key-value pair struct (our "index" entry)
+struct Entry {
+    std::string key;
+    std::string value;
+};
